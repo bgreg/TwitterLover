92b06dc894723888aa91b05c0d96257c0332f5ad
greg@vorstack.com <bgregmc@icloud.com>
2014-04-08 20:23:44 -0700

    initial commit.  Gems installed, no generators ran

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterLover::Application.config.secret_token = 'c5d0059c60485ccb24d6ca3c515859eca88f9936751d27ec6ff7a1e0f2508156835e54ca3c73600d4595123b7398e8ed558d8d35464ebf151e994a7fb9ee044c'
