935dfc5a3cb057efcb9386c7b250734a389be766
greg@vorstack.com <bgregmc@icloud.com>
2014-04-10 14:12:48 -0700

    added more tests I forgot earlier

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe Tweet do
+  # sanity check
   it "should be able to instatiate a tweet object" do
     tweet = Tweet.new
     tweet.should be_an_instance_of( Tweet )
@@ -9,4 +10,9 @@ describe Tweet do
   it { should validate_presence_of(:user_name) }
   it { should validate_presence_of(:geo_coordinates) }
   it { should validate_presence_of(:text) }
+  it { should have_index_for(geo_coordinates: '2d')}
+
+  it { should have_field(:user_name).of_type(String)}
+  it { should have_field(:geo_coordinates).of_type(Array)}
+  it { should have_field(:text).of_type(String)}
 end
