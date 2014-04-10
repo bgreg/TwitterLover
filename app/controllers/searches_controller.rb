17f0ab28f2b5811ef807f0d0b36a83e1b7d8ae12
greg@vorstack.com <bgregmc@icloud.com>
2014-04-10 14:30:02 -0700

    form is working, tests passed

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,4 +1,7 @@
 class SearchesController < ApplicationController
   def new
   end
+  def find_me_friends
+    redirect_to :back
+  end
 end
--- spec/features/new_search_page_spec.rb
+++ spec/features/new_search_page_spec.rb
@@ -1,38 +1,66 @@
 # For debugging: save_and_open_page
 
 require 'spec_helper'
-describe "appearance of new_search page" do 
-  before do
-    visit '/searches/new'
-  end
-  it "should have text 'welcome to twitter lover, where you can meet other twitter lovers near you'" do
-    page.should have_content('welcome to twitter lover, where you can meet other twitter lovers near you')
+describe "new_search page" do 
+  describe "appearance" do
+    before{visit '/searches/new'}
+
+    it "should have text 'welcome to twitter lover, where you can meet other twitter lovers near you'" do
+      page.should have_content('welcome to twitter lover, where you can meet other twitter lovers near you')
+    end
+
+    it "should hvae a label for long" do 
+      page.should have_content( 'Longitude' )
+    end
+    it "should display a text box for entering logitude of your current location" do
+      page.should have_css( 'input#long')
+    end
+
+    it "should hvae a label for lat" do 
+      page.should have_content( 'Latitude' )
+    end
+    it "should display a text box for entering logitude of your current location" do
+      page.should have_css( 'input#lat')
+    end
+
+    it "should display have a submit button" do
+      page.should have_css( 'input#submit')
+    end
+
+    it "should tell the user what to do" do
+      page.should have_content("Enter your location, and a radius and I will give you tweets in that circle")
+    end
+
+    it "should hvae a label for radius" do 
+      page.should have_content( 'Radius' )
+    end
+    it "should display a text box for entering logitude of your current location" do
+      page.should have_css( 'input#rad')
+    end
+
+    it "should have a row" do
+      page.should have_css( 'div.row')
+    end
   end
 
-  it "should hvae a label for long" do 
-    page.should have_content( 'Longitude' )
-  end
-  it "should display a text box for entering logitude of your current location" do
-    page.should have_css( 'input#long')
-  end
+  describe "form" do
+    before{visit '/searches/new'}
+    it "should not accept strings in the longitude field" do 
+      fill_in "lat", with: "string example"
+      click_button :submit
+      page.should_not have_content("query submitted")
+    end
 
-  it "should hvae a label for lat" do 
-    page.should have_content( 'Latitude' )
-  end
-  it "should display a text box for entering logitude of your current location" do
-    page.should have_css( 'input#lat')
-  end
+    it "should not accept strings in the longitude field" do 
+      fill_in :long, with: "string example"
+      click_button :submit
+      page.should_not have_content("query submitted")
+    end
 
-  it "should display have a submit button" do
-    page.should have_css( 'input#submit')
+    it "should not accept strings in the longitude field" do 
+      fill_in :rad, with: "string example"
+      click_button :submit
+      page.should_not have_content("query submitted")
+    end
   end
-
-  it "should tell the user what to do" do
-    page.should have_content("Enter your location, and a radius and I will give you tweets in that circle")
-  end
-
-  it "should have a row" do
-    page.should have_css( 'div.row')
-  end
-
 end
