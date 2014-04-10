e99dd69c4b9b0d72a8b61d5cf30b282f9104b9a2
greg@vorstack.com <bgregmc@icloud.com>
2014-04-10 00:17:10 -0700

    This can be setup once at init, moved out of rake task

---
+++ config/initializers/tweetstream_init.rb
@@ -0,0 +1,7 @@
+TweetStream.configure do |config|
+  config.consumer_key       = '3GtoyQKKkuCu5dOJXy7wEUW27'
+  config.consumer_secret    = 'U4gyU8ATi8UrKGJc6mJq5wEZEBtCssni4Uym0AZOlfckalLUra'
+  config.oauth_token        = '1012371834-eIcrn59ZMPN51vQtrghROSGJrdplKP9rgSr8mgU'
+  config.oauth_token_secret = '1L1bSwpWTIM9DMUNLREawG5n5CtjpB6bJzo0G8pChyGks'
+  config.auth_method        = :oauth
+end
