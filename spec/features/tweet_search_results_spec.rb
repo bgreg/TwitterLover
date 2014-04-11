aa55e2db550f6cbd97cfb80ba4894db6862db984
greg@vorstack.com <bgregmc@icloud.com>
2014-04-10 23:37:16 -0700

    visit url should not of used localhost

--- spec/features/tweet_search_results_spec.rb
+++ spec/features/tweet_search_results_spec.rb
@@ -2,12 +2,10 @@ require 'spec_helper'
 
 describe "results" do 
   describe "form" do
-    before{ visit 'http://localhost:3000/searches/tweet_search_results?lat=1&long=1&page=2&rad=200' }
-    it "with max radius it should show me page links", js: true do
-      page.should have_content("First")
-      page.should have_content("Prev")
+    before{ visit '/searches/tweet_search_results?lat=1&long=1&page=2&rad=200' }
+
+    it "with max radius it should show me a next page links", js: true do
       page.should have_content("Next")
-      page.should have_content("Last")
     end
 
     it "should have a title", js: true do 
