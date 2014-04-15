9108af2db24bcc076443611f1cca8b76bfeac701
greg@vorstack.com <bgregmc@icloud.com>
2014-04-14 19:33:09 -0700

    small refactor

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,17 +3,18 @@ class SearchesController < ApplicationController
   end
 
   def tweet_search_results
-    lat  = params[:lat].to_i
-    long = params[:long].to_i
-    rad  = params[:rad].to_i
+    lat, long, rad = get_search
 
     @my_tweets = Tweet.within_circle( geo_coordinates: [[lat,long],rad]).order_by(created_at: :dec).page params[:page]
 
-    if @my_tweets
-      message = { notice: "query accepted" }
-    else
-      message = { notice: "no tweets found" }
+    @tweet_locations = []
+    @my_tweets.each do |t|
+      @tweet_locations << [t.geo_coordinates[0],t.geo_coordinates[1]]
     end
-    render tweet_search_results_searches_path, message and return
+    render tweet_search_results_searches_path
+  end
+private
+  def get_search
+    return params[:lat].to_i, params[:long].to_i, params[:rad].to_i
   end
 end
